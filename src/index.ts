96148f7e9692f56fb917794d40e21009e167794d
Edwin <digiaries@hotmail.com>
2021-02-03 10:23:40 +0800

    feat: 手动导出

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-export * from "./is";
-export * from "./utils";
-export * from "./math";
+export {
+    labelReplaceExp
+    ,labelReplace
+    ,isValidArray
+    ,toArray
+    ,each
+    ,extend
+    ,merge
+    ,toUnderlineName
+    ,copy
+    ,fix0
+    ,numberFormat
+    ,shuffle
+    ,random
+    ,serialize
+    ,queryString
+    ,toDate
+    ,date
+} from "./utils";
+export {
+    is
+    ,isUndefined
+    ,isString
+    ,isRegexp
+    ,isObject
+    ,isNumber
+    ,isNull
+    ,isFunction
+    ,isError
+    ,isBoolean
+    ,isArray
+} from "./is";
+export {
+    getNumberWithDec
+    ,gettNumberAverage
+    ,gettNumberVariance
+    ,getStdDeviation
+} from "./math";
--- src/utils.ts
+++ src/utils.ts
@@ -21,22 +21,22 @@ export { labelReplaceExp }
  */
 export function labelReplace<T>(tpl: string, data: T, keep: boolean = false): string {
     return tpl.replace(labelReplaceExp, function (_, key) {
-        const re = isObject(data) ? data[key] : data
+        const re = isObject(data) ? data[key] : data;
         if (isUndefined(re) && keep) {
-            return _
+            return _;
         }
-        return re
+        return re;
     })
 }
 
-const A_SLICE: Function = Array.prototype.slice
+const A_SLICE: Function = Array.prototype.slice;
 
 /**
  * 是否是数组且长度大于0
  * @param  subject 待判断的数据
  */
 export function isValidArray(subject: any): boolean {
-    return Array.isArray(subject) && subject.length > 0
+    return Array.isArray(subject) && subject.length > 0;
 }
 
 /**
@@ -45,7 +45,7 @@ export function isValidArray(subject: any): boolean {
  * @return       转化后的数组
  */
 export function toArray<T>(args: T[]): Array<T> {
-    return Array.prototype.slice.call(args)
+    return Array.prototype.slice.call(args);
 }
 
 /**
@@ -55,27 +55,27 @@ export function toArray<T>(args: T[]): Array<T> {
  * @param  context 作用域
  */
 export function each<T>(data: T[] | object, handler: Function, context?: object): void {
-    context = context || this
-    var hasHandler = isFunction(handler)
+    context = context || this;
+    var hasHandler = isFunction(handler);
     if (isArray(data)) {
         for (let i = 0; i < (data as T[]).length; i++) {
-            let re = true
+            let re = true;
             if (hasHandler) {
-                re = handler.call(context, data[i], i)
+                re = handler.call(context, data[i], i);
             }
             if (re === false) {
-                break
+                break;
             }
         }
     } else if (isObject(data)) {
-        const keys = Object.keys(data)
+        const keys = Object.keys(data);
         for (let i = 0; i < keys.length; i++) {
-            let re = true
+            let re = true;
             if (hasHandler) {
-                re = handler.call(context, data[keys[i]], keys[i])
+                re = handler.call(context, data[keys[i]], keys[i]);
             }
             if (re === false) {
-                break
+                break;
             }
         }
     }
@@ -84,7 +84,7 @@ export function each<T>(data: T[] | object, handler: Function, context?: object)
 /**
  * 大写字母匹配正则
  */
-const UPPER_CASE_REGEXP: RegExp = /[A-Z]/g
+const UPPER_CASE_REGEXP: RegExp = /[A-Z]/g;
 
 /**
  * 转化为蛇形命名
@@ -93,9 +93,9 @@ const UPPER_CASE_REGEXP: RegExp = /[A-Z]/g
  */
 export function toUnderlineName(str: string): string {
     if (isString(str)) {
-        return str.replace(UPPER_CASE_REGEXP, (m, i) => `${i ? '_' : ''}${m.toLowerCase()}`)
+        return str.replace(UPPER_CASE_REGEXP, (m, i) => `${i ? '_' : ''}${m.toLowerCase()}`);
     }
-    return str
+    return str;
 }
 
 /**
@@ -104,7 +104,7 @@ export function toUnderlineName(str: string): string {
  * @return      复制后的数据
  */
 export function copy<T>(item: T): T {
-    return JSON.parse(JSON.stringify(item))
+    return JSON.parse(JSON.stringify(item));
 }
 
 /**
@@ -114,11 +114,11 @@ export function copy<T>(item: T): T {
  * @return        格式化结果
  */
 export function fix0(number, size: number): string {
-    number = number.toString()
+    number = number.toString();
     while ((number).length < size) {
-        number = '0' + number
+        number = '0' + number;
     }
-    return number
+    return number;
 }
 
 /**
@@ -128,12 +128,12 @@ export function fix0(number, size: number): string {
  */
 export function shuffle<T>(arr: T[]): T[] {
     for (let i = arr.length - 1; i >= 0; i--) {
-        const rIndex = Math.floor(Math.random() * (i + 1))
-        const item = arr[rIndex]
-        arr[rIndex] = arr[i]
-        arr[i] = item
+        const rIndex = Math.floor(Math.random() * (i + 1));
+        const item = arr[rIndex];
+        arr[rIndex] = arr[i];
+        arr[i] = item;
     }
-    return arr
+    return arr;
 }
 
 /**
@@ -144,9 +144,9 @@ export function shuffle<T>(arr: T[]): T[] {
  */
 export function random(max: number, min: number): number {
     if (min === undefined) {
-        min = 0
+        min = 0;
     }
-    return Math.round(Math.random() * (max - min) + min)
+    return Math.round(Math.random() * (max - min) + min);
 }
 
 /**
@@ -165,23 +165,23 @@ export function serialize(query: object, encode: boolean = false): string {
  * @return 合并后的对象
  */
 export function extend() {
-    var args: any[] = A_SLICE.call(arguments)
-    var dat = args.shift()
+    var args: any[] = A_SLICE.call(arguments);
+    var dat = args.shift();
     if (dat) {
         if (args.length) {
             while (args.length) {
-                const item = args.shift()
+                const item = args.shift();
                 if (item) {
                     each(item, function (val, key) {
-                        dat[key] = val
+                        dat[key] = val;
                     })
                 }
             }
         }
-        return dat
+        return dat;
     }
 
-    return null
+    return null;
 }
 
 /**
@@ -190,8 +190,8 @@ export function extend() {
  * @param sources 后续合并对象
  */
 export function merge(target: object, ...sources: any[]) {
-    if (!sources.length) return target
-    const source: any = sources.shift()
+    if (!sources.length) return target;
+    const source: any = sources.shift();
     if (isObject(target) && isObject(source)) {
         for (const key in source) {
             if (isObject(source[key])) {
@@ -223,11 +223,11 @@ export function queryString(dat: { [key: string]: any }): string {
             .map(key => {
                 return `${key}=${encodeURIComponent(dat[key])}`
             })
-            .join('&')
+            .join('&');
     } else {
-        queryStr = ''
+        queryStr = '';
     }
-    return queryStr
+    return queryStr;
 }
 
 /**
