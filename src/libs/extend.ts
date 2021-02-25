f72f35536887f15108a79eb928a387ea99a63c0f
Edwin <digiaries@hotmail.com>
2021-02-25 16:10:36 +0800

    feat: extend 使用 rest 写法，不再使用 arguments 转成数组

--- src/libs/extend.ts
+++ src/libs/extend.ts
@@ -1,12 +1,10 @@
-import toArray from "./toArray";
 import each from "./each";
 
 /**
  * 对象扩展方法
  * @return 合并后的对象
  */
-export default function extend() {
-    var args: any[] = toArray(arguments);
+export default function extend(...args) {
     var dat = args.shift();
     if (dat) {
         if (args.length) {
