cfcb588712624d3979f98442c6dd22d08595dd06
Edwin <digiaries@hotmail.com>
2021-03-31 11:55:43 +0800

    fix: 修复设置 cookie 操作类型及配置项处理问题

--- src/libs/cookie.ts
+++ src/libs/cookie.ts
@@ -1,19 +1,33 @@
 import extend from "./extend";
 
+/**Cookie 配置 */
+interface CookieOption {
+    /**cookie 生效地址 */
+    path?: string;
+
+    /**过期时间 */
+    expires?: number;
+}
+
+/**过期 */
 const EXP_STR = "Thu, 01 Jan 1970 00:00:00 GMT";
 
-// cookie写操作，过期时间单位(s)
-function set(name, value, config) {
-    config = extend(
-        {
-            "path": "/"
-        }
-        ,config || {}
-    )
+const DEF_CONFIG = {
+    "path": "/"
+}
 
-    const defVal = {
-        "path": "/"
-    }
+/**
+ * cookie写操作，过期时间单位(s)
+ * @param name   cookie key
+ * @param value  cookie value
+ * @param config cookie 配置
+ */
+function set(name: string, value, config: CookieOption) {
+    config = extend(
+        {}
+        , DEF_CONFIG
+        ,config || {}
+    );
 
     var cookie = [`${name}=${value}`];
     Object.keys(config).forEach(function (key) {
@@ -21,18 +35,27 @@ function set(name, value, config) {
             return cookie.push("expires=" + new Date(+new Date() + config.expires * 24 * 3600 * 1000).toUTCString());
         }
         cookie.push(
-            `${key}=${config[key] || defVal[key] || ""}`
+            `${key}=${config[key] || ""}`
         );
-    })
-    document.cookie = cookie.join(";");
+    });
+    document.cookie = cookie.join("; ");
 }
 export { set };
 
+/**
+ * 获取指定的 cookie
+ * @param name cookie 名称
+ */
 function get(name:string) {
     return document.cookie.replace(new RegExp(".*(?:^|; )" + name + "=([^;]*).*|.*"), "$1");
 }
 export { get }
 
+/**
+ * 删除 cookie
+ * @param name 要删除的 cookie 名称
+ * @param path 生效路径
+ */
 function remove(name: string, path: string) {
     path = path || "/";
     var value = get(name);
