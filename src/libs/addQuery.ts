b9ba7d9bc84e9d225ba38708a9bdad74b0c3d2e5
Edwin <digiaries@hotmail.com>
2021-03-31 11:59:57 +0800

    fix: 修复 addQuery 在 url 只有一个问号结尾的时候的拼接问题

--- src/libs/addQuery.ts
+++ src/libs/addQuery.ts
@@ -1,10 +1,18 @@
 import serialize from "./serialize";
 import isObject from "./isObject";
 
+/**只有一个问号结尾 */
+const IS_QM_ONLY_REGEXP = /\?$/;
+
+/**
+ * 往 url 中附加参数
+ * @param url   目标 url
+ * @param query 需要附加的参数
+ */
 export default function addQuery(url:string, query:any) {
     var extQueryStr = "";
     if (isObject(query)) {
        extQueryStr = serialize(query); 
     }
-    return `${url}${url.indexOf("?") !== -1 ? "&" : "?"}${extQueryStr}`;
+    return `${url}${url.indexOf("?") !== -1 ? IS_QM_ONLY_REGEXP.test(url) ? "" : "&" : "?"}${extQueryStr}`;
 }
