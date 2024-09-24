c9399cf44f2e7695f52aa04e520b4d35fe693db4
Edwin <digiaries@hotmail.com>
2024-09-24 18:07:19 +0800

    fix: 复制到剪切板功能支持非 https 环境

--- src/libs/copyToClipboard.ts
+++ src/libs/copyToClipboard.ts
@@ -3,17 +3,30 @@
  * @param text 待写入剪切板的内容
  */
 async function copyToClipboard(text: string) {
-    if (!navigator || !navigator.clipboard) {
-
-        return Promise.reject(new Error("不支持当前环境, 请确定运行的浏览器类型及版本"));
-    }
-    return navigator.clipboard.writeText(text)
-        .then(() => {
-            return true;
+    if (navigator.clipboard) {
+        return navigator.clipboard.writeText(text)
+            .then(() => {
+                return true;
+            })
+            .catch(() => {
+                return false;
+            });
+    } else if (document.execCommand) {
+        return new Promise<boolean>(res => {
+            let input = document.createElement("textarea"); // 使用textarea文本才能换行
+            // 设置内容
+            input.value = text;
+            // 添加临时实例
+            document.body.appendChild(input);
+            // 选择实例内容
+            input.select();
+            document.execCommand("copy");
+            document.body.removeChild(input);
+            res(true);
         })
-        .catch(() => {
-            return false;
-        });
+    } else {
+        return Promise.reject(new Error("不支持当前环境, 请确定运行的浏览器类型及版本"));
+    }    
 }
 
 export default copyToClipboard;
